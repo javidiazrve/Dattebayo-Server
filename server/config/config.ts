c7492d5b40ba97a9120b78c9ad317df855d85eb9
unknown <javierzaneti999@gmail.com>
2020-11-30 15:12:17 -0500

    heroku deploy

--- server/config/config.ts
+++ server/config/config.ts
@@ -2,7 +2,7 @@
 //  PUERTO
 //=======================
 
-process.env.PORT = process.env.port || '3000';
+process.env.PORT = process.env.PORT || '3000';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
